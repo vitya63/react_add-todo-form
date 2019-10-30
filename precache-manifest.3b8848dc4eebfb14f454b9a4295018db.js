cb6ef43956eaf44ea0bf346ed2ed453eb433079f
Viktor Lukovets <nuerio63@gmail.com>
2019-10-30 17:40:44 +0200

    Updates

---
+++ precache-manifest.3b8848dc4eebfb14f454b9a4295018db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460db591f70017d4a27d1f474bca4497",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "655e39f61dd2bc2e79d9",
+    "url": "/react_add-todo-form/static/css/main.a2cfc491.chunk.css"
+  },
+  {
+    "revision": "1b2cfe77f2bb9f146d39",
+    "url": "/react_add-todo-form/static/js/2.3c77df80.chunk.js"
+  },
+  {
+    "revision": "655e39f61dd2bc2e79d9",
+    "url": "/react_add-todo-form/static/js/main.252c2ed0.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
